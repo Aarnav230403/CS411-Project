81655de30b1407e762a7bc9fea5e0857b4e311b2
Aarnav230403 <145166076+Aarnav230403@users.noreply.github.com>
2025-04-28 20:24:29 -0400

    Update conftest.py

--- catalog/tests/conftest.py
+++ catalog/tests/conftest.py
@@ -1,12 +1,13 @@
 import pytest
 
 from app import create_app
-from config import TestingConfig
-from catalog.config import TestConfig
+from config import TestConfig
+from catalog.models.user_model import Users  # optional depending on your tests
+from catalog.sql.create_db import db  # <-- Make sure this is right path to your db object
 
 @pytest.fixture
 def app():
-    app = create_app(config_class=TestingConfig)
+    app = create_app(config_class=TestConfig)
     with app.app_context():
         db.create_all()
         yield app
