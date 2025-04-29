49835954c037164c6b0fd42417331bd795348c86
Aarnav230403 <145166076+Aarnav230403@users.noreply.github.com>
2025-04-28 20:03:29 -0400

    Update conftest.py

--- catalog/tests/conftest.py
+++ catalog/tests/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 
 from app import create_app
-class TestingConfig(ProductionConfig):
+from config import TestingConfig
 from playlist.db import db
 
 @pytest.fixture
 def app():
-    app = create_app(TestConfig)
+    app = create_app(config_class=TestingConfig)
     with app.app_context():
         db.create_all()
         yield app
